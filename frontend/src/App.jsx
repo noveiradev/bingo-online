2dd762df57fd5f9193875b6f15ecbb0b6e33b8eb
REFH4CK <refh4ck.dev@gmail.com>
2025-07-16 15:14:53 -0400

    Protected routes with role authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthRoute } from "@/components/AuthRoute";
+
 import Header from "@/components/Header.jsx";
 import Footer from "@/components/Footer.jsx";
 import Login from "@/pages/Login.jsx";
 import Register from "@/pages/Register.jsx";
 import ForgotPassword from "@/pages/ForgotPassword.jsx";
+
+import Dashboard from "@/pages/Dashboard.jsx";
 
 export default function App() {
   return (
@@ -16,26 +20,67 @@ export default function App() {
               // Routes with access without authentication
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
-              <Route
-                path="/forgot-password"
-                element={<ForgotPassword />}
-              />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
               // Routes need (User/Admin role) to access
-              <Route path="/dashboard" element={"Menu"} />
-              <Route path="/cardboards" element={"Tus cartones"} />
-              <Route path="/reserve-cardboard" element={"Apartar cartones"} />
-              <Route path="/game-rules" element={"Reglas del juego"} />
-              <Route path="/settings" element={"Configuracion"} />
+              <Route
+                path="/dashboard"
+                element={
+                  <AuthRoute requiredRoles={["user", "admin"]}>
+                    <Dashboard />
+                  </AuthRoute>
+                }
+              />
+              <Route
+                path="/cardboards"
+                element={
+                  <AuthRoute requiredRoles={["user", "admin"]}></AuthRoute>
+                }
+              />
+              <Route
+                path="/reserve-cardboard"
+                element={
+                  <AuthRoute requiredRoles={["user", "admin"]}></AuthRoute>
+                }
+              />
+              <Route
+                path="/game-rules"
+                element={
+                  <AuthRoute requiredRoles={["user", "admin"]}></AuthRoute>
+                }
+              />
+              <Route
+                path="/settings"
+                element={
+                  <AuthRoute requiredRoles={["user", "admin"]}></AuthRoute>
+                }
+              />
+              <Route
+                path="/game/room:id"
+                element={
+                  <AuthRoute requiredRoles={["user", "admin"]}></AuthRoute>
+                }
+              />
               // Routes need Only (Admin role) to access
-              <Route path="/verify-cardboards" element={"Validar cartones"} />
-              <Route path="/pay-information" element={"Pago movil"} />
-              <Route path="/pay-information:id" element={"Editar pago movil"} />
-              <Route path="/game-patterns" element={"Modalidades/Patrones"} />
+              <Route
+                path="/verify-cardboards"
+                element={<AuthRoute requiredRoles={["admin"]}></AuthRoute>}
+              />
+              <Route
+                path="/pay-information"
+                element={<AuthRoute requiredRoles={["admin"]}></AuthRoute>}
+              />
+              <Route
+                path="/pay-information:id"
+                element={<AuthRoute requiredRoles={["admin"]}></AuthRoute>}
+              />
+              <Route
+                path="/game-patterns"
+                element={<AuthRoute requiredRoles={["admin"]}></AuthRoute>}
+              />
               <Route
                 path="/create-pattern"
-                element={"Crear Modalidades/Patrones"}
+                element={<AuthRoute requiredRoles={["admin"]}></AuthRoute>}
               />
-              <Route path="/game/room:id" element={"Partida en Directo"} />
             </Routes>
           </main>
           <Footer />
