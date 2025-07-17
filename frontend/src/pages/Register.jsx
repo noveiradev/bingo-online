535c9663f54ffc86ecc23aff277699166c31e25e
REFH4CK <refh4ck.dev@gmail.com>
2025-07-17 13:11:21 -0400

    add register service

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -4,7 +4,7 @@ import Input from "@/components/Input";
 import SelectList from "@/components/SelectList";
 import Button from "@/components/Button";
 
-import { registerService } from '@/services/api/auth';
+import { registerService } from "@/services/api/auth";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -25,8 +25,8 @@ export default function Register() {
 
   const navigate = useNavigate();
 
-const onSubmit = async (data) => {
-     try {
+  const onSubmit = async (data) => {
+    try {
       const userData = {
         username: data.username,
         password: data.password,
@@ -37,16 +37,14 @@ const onSubmit = async (data) => {
 
       const response = await registerService.register(userData);
 
-    if (response) {
-      setMessage("Registro exitoso!");
-      setTimeout(() => {
-        navigate('/login', { state: { registrationSuccess: true } });
-      }, 2000);
-    }
-
-
+      if (response) {
+        setMessage("Registro exitoso!");
+        setTimeout(() => {
+          navigate("/login", { state: { registrationSuccess: true } });
+        }, 2000);
+      }
     } catch (error) {
-      console.error('Registration error:', error); 
+      console.error("Registration error:", error);
     }
   };
 
@@ -74,7 +72,7 @@ const onSubmit = async (data) => {
               register={register}
               error={errors.username}
               {...register("username", {
-                required: "",
+                required: "Por favor llena todos los campos",
               })}
             >
               <User w={"22"} h={"22"} />
@@ -87,7 +85,7 @@ const onSubmit = async (data) => {
               register={register}
               error={errors.phone}
               {...register("phone", {
-                required: "",
+                required: "Por favor llena todos los campos",
               })}
             >
               <Phone w={"22"} h={"22"} />
@@ -100,7 +98,7 @@ const onSubmit = async (data) => {
               register={register}
               error={errors.password}
               {...register("password", {
-                required: "",
+                required: "Por favor llena todos los campos",
                 minLength: {
                   value: 8,
                   message: "La contraseña debe tener al menos 8 caracteres",
@@ -115,7 +113,7 @@ const onSubmit = async (data) => {
               id="securityQuestion"
               error={errors.securityQuestion}
               {...register("securityQuestion", {
-                required: "",
+                required: "Por favor llena todos los campos",
               })}
             >
               <Question w={"22"} h={"22"} />
@@ -128,7 +126,7 @@ const onSubmit = async (data) => {
               register={register}
               error={errors.answer}
               {...register("answer", {
-                required: "",
+                required: "Por favor llena todos los campos",
               })}
             >
               <Answer w={"22"} h={"22"} />
@@ -140,11 +138,7 @@ const onSubmit = async (data) => {
               text="Registrarse"
               className="text-white w-[90%] mx-auto font-semibold py-2 px-4 rounded-[7px] mt-4 hover:bg-yellow-cake/80 transition-colors duration-200 bg-linear-to-t from-[#794d10] to-[#D46613]"
             />
-            {message && (
-              <p className="text-gold text-center mt-2">
-                {message}
-              </p>
-            )}
+            {message && <p className="text-gold text-center mt-2">{message}</p>}
           </article>
         </form>
       </section>
