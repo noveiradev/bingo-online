f46007aac5f3278137756f69e1a83d9b7e125584
REFH4CK <refh4ck.dev@gmail.com>
2025-07-18 16:53:40 -0400

    Unathorized structure page

--- frontend/src/pages/Unauthorized.jsx
+++ frontend/src/pages/Unauthorized.jsx
@@ -1,4 +1,5 @@
 import GoBack from "@/components/GoBack";
+import BingoRoller from "@/assets/images/bingo.webp";
 
 export default function Unauthorized() {
   return (
@@ -7,7 +8,16 @@ export default function Unauthorized() {
         <div className="flex w-full max-w-[500px] px-2">
           <GoBack></GoBack>
         </div>
+        <article className="flex flex-col gap-2 justify-center items-center p-4 py-8">
+          <h2 className="text-dark-gold mt-4 text-center font-semibold font-poppins text-2xl">
+            No tienes permisos para entrar a esta ruta
+          </h2>
+          <img src={BingoRoller} alt="Bingo roller" className="size-[18rem] drop-shadow-[0_0_5px_rgba(83,63,27,0.80)]" />
+          <p className="text-[#FAF2E7] text-center font-poppins">
+            Por favor, utiliza el boton de Volver para dirigirte a la pagina anterior.
+          </p>
+        </article>
       </section>
     </>
-  )
+  );
 }
