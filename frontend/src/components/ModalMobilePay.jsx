297c48c67d2592cb88bdfc648970f7585fcb6914
REFH4CK <refh4ck.dev@gmail.com>
2025-07-22 00:36:50 -0400

    add other colors and fix modal distribution

--- frontend/src/components/ModalMobilePay.jsx
+++ frontend/src/components/ModalMobilePay.jsx
@@ -1,25 +1,32 @@
+import BankCenter from "@/icons/BankCenter";
+import { Slide } from "react-awesome-reveal";
+
 export default function ModalMobilePay({ modalFunction, payData }) {
   return (
     <>
-      <section className="absolute z-10 top-0 left-0 w-full h-dvh bg-[#000]/60">
-        <article className="max-w-[350px] w-[95%] h-[25rem] bg-white/10 mx-auto mt-24 backdrop-blur-lg rounded-2xl shadow-[#000]/50 shadow-lg outline-[#4E4E4B] outline-3 font-poppins">
-          <div className="p-2 py-5 flex flex-col gap-2">
-            <h1 className="text-center font-bold text-[1.6rem] text-[#FAF2E7]">
-              Datos de pago móvil
-            </h1>
-            <p className="text-[#FAF2E7] text-center text-md"></p>
-          </div>
-          <span className="flex justify-center my-1"></span>
-          <div className="p-2 py-5 flex flex-col gap-2">
-            <span
-              onClick={() => modalFunction(!payData)}
-              className="bg-deep-blue text-center px-10 py-2 text-white rounded-md shadow-[#000]/40 shadow-md cursor-pointer"
-            >
-              Cerrar
-            </span>
-          </div>
-        </article>
-      </section>
+      <Slide direction="up" triggerOnce duration={600} className="absolute z-10 top-0 left-0 w-full h-dvh" >
+        <section className="bg-[#000]/60">
+          <article className="max-w-[350px] w-[95%] h-[25rem] bg-white/10 mx-auto mt-24 backdrop-blur-xl rounded-2xl shadow-[#000]/50 shadow-lg outline-[#4E4E4B] outline-3 font-poppins">
+            <div className="p-4 py-5 flex flex-col gap-4">
+              <h1 className="text-center font-bold text-[1.6rem] text-[#FAF2E7]">
+                Datos de pago móvil
+              </h1>
+              <div className="mx-auto pt-4">
+                <BankCenter w="90" h="90" color="#FAF2E795" />
+              </div>
+              <div className="w-full bg-white/8 h-[8rem] rounded-xl outline-simple-gold/50 outline-3"></div>
+            </div>
+            <div className="px-4 flex flex-col">
+              <span
+                onClick={() => modalFunction(!payData)}
+                className="bg-deep-blue text-center px-10 py-2 text-white rounded-md shadow-[#000]/40 shadow-md cursor-pointer"
+              >
+                Cerrar
+              </span>
+            </div>
+          </article>
+        </section>
+      </Slide>
     </>
   );
 }
