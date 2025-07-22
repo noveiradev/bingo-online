3d46316515db3ba92f24dce077c375a770ecb1ab
REFH4CK <refh4ck.dev@gmail.com>
2025-07-22 00:38:10 -0400

    add cardSigns to show informative icons

--- frontend/src/components/Cards/OwnedCardboard.jsx
+++ frontend/src/components/Cards/OwnedCardboard.jsx
@@ -4,30 +4,40 @@ import { toast } from "@pheralb/toast";
 import Button from "@/components/Button";
 import { cancelReserve } from "@/services/api/cards";
 
-
-export default function OwnedCardboard({ cardNumber, numbersArray }) {
+export default function OwnedCardboard({
+  cardNumber,
+  pay,
+  is_gift,
+  numbersArray,
+}) {
   const { user } = useAuth();
+
+  let gift = is_gift === 1 ? "gift" : "";
+
+  const cardSigns = {
+    paid: "✅",
+    pending: "❎",
+    gift: "🎁",
+  };
 
   const numbers = Array.isArray(numbersArray)
     ? numbersArray
     : JSON.parse(numbersArray.replace(/free/g, '"free"'));
 
-  const handleCancelReserve = async (cardId) => {
+  const handleCancelReserve = async (cardId, event) => {
     try {
       const userData = { id: user.id, cardId };
       const response = await cancelReserve.cancelReserve(userData);
 
       if (response.success) {
         toast.success({
-          text: response.message
+          text: response.message,
         });
 
-        setTimeout(() => {
-          window.location.reload();
-        }, 1500);
+        event.target.closest("article").remove();
       } else {
         toast.error({
-          text: response.message
+          text: response.message,
         });
       }
     } catch (error) {
@@ -36,9 +46,9 @@ export default function OwnedCardboard({ cardNumber, numbersArray }) {
   };
   return (
     <>
-      <article className="flex flex-col items-center bg-white/10 p-3 py-2 rounded-xl h-fit">
-        <h2 className="text-md font-poppins font-semibold text-[#FFC62C]">
-          Carton N° {cardNumber}
+      <article className="flex flex-col items-center bg-white/10 p-2 py-2 rounded-xl h-fit">
+        <h2 className="text-sm mb-1 font-poppins font-semibold text-[#FFC62C]">
+          Carton N° {cardNumber} {cardSigns[gift || pay]}
         </h2>
         <div className="grid grid-cols-5 grid-rows-5 w-[112px] h-[140px] px-[0.6rem] pb-[1rem] relative pt-[1.85rem]">
           {numbers.map((number, index) => (
@@ -60,13 +70,14 @@ export default function OwnedCardboard({ cardNumber, numbersArray }) {
           />
         </div>
         <div className="flex gap-2">
-          {/* <button className="bg-green-apple/63 text-white mt-2 font-medium px-2 py-1 rounded-md text-center text-sm w-full">
-            Descargar
-          </button> */}
           <Button
             text="Cancelar"
-            className="bg-coral-red text-white mt-2 font-medium px-2 py-1 rounded-md text-center text-sm w-full cursor-pointer"
-            onClick={() => handleCancelReserve(cardNumber)}
+            className={`${pay === "pending" ? "bg-coral-red text-white" : "bg-coral-red/50 text-white/50"}  mt-2 font-medium px-2 py-1 rounded-md text-center text-sm w-full cursor-pointer`}
+            onClick={
+              pay === "pending"
+                ? (e) => handleCancelReserve(cardNumber, e)
+                : () => {}
+            }
           />
         </div>
       </article>
