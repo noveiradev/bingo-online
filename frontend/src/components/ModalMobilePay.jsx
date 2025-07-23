e4109b80a7009191500e2eaaa30846b2a537cf78
REFH4CK <refh4ck.dev@gmail.com>
2025-07-23 15:47:50 -0400

    delete modal overlay

--- frontend/src/components/ModalMobilePay.jsx
+++ frontend/src/components/ModalMobilePay.jsx
@@ -5,8 +5,8 @@ export default function ModalMobilePay({ modalFunction, payData }) {
   return (
     <>
       <Slide direction="up" triggerOnce duration={600} className="absolute z-10 top-0 left-0 w-full h-dvh" >
-        <section className="bg-[#000]/60">
-          <article className="max-w-[350px] w-[95%] h-[25rem] bg-white/10 mx-auto mt-24 backdrop-blur-xl rounded-2xl shadow-[#000]/50 shadow-lg outline-[#4E4E4B] outline-3 font-poppins">
+        <section className="">
+          <article className="max-w-[350px] w-[95%] h-[25rem] bg-white/10 mx-auto mt-24 backdrop-blur-2xl rounded-2xl shadow-[#000]/50 shadow-lg outline-[#4E4E4B] outline-3 font-poppins">
             <div className="p-4 py-5 flex flex-col gap-4">
               <h1 className="text-center font-bold text-[1.6rem] text-[#FAF2E7]">
                 Datos de pago móvil
