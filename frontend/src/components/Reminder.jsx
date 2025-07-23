46419c823a2192b9111eac44e3cdf3191e651ad0
REFH4CK <refh4ck.dev@gmail.com>
2025-07-23 15:48:45 -0400

    Add stable breakpoint in the reminder advice

--- frontend/src/components/Reminder.jsx
+++ frontend/src/components/Reminder.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 export default function Reminder({children}) {
   return (
     <>
-      <article className="bg-reminder/37 w-[300px] text-white/95 text-center p-2 rounded-lg shadow-md mt-4 text-[0.8rem] font-poppins">
+      <article className="bg-reminder/37 w-[95%] max-w-[500px] text-white/95 text-center p-2 rounded-lg shadow-md mt-4 text-[0.8rem] font-poppins stable:text-[0.95rem] stable:w-[24rem]">
         {children}
       </article>
     </>
