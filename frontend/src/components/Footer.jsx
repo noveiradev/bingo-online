81011edba03bebc6125f99ad0375fa75dc6c13ea
REFH4CK <refh4ck.dev@gmail.com>
2025-07-24 19:32:22 -0400

    add stable breakpoint

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import CasinoLights from "./CasinoLights";
 export default function Footer() {
   return (
     <footer className="relative z-20 bottom-0 flex flex-col justify-center items-center w-full h-[5rem] bg-borgon text-center text-white/85 p-2">
-      <p className="text-[0.85rem]">
+      <p className="text-[0.85rem] stable:text-[0.95rem]">
         Pagina desarrollada por <b>noveiradev</b> y <b>REFH4CK</b>
       </p>
       <p className="text-[0.9rem]">Todos los derechos reservados © 2025</p>
