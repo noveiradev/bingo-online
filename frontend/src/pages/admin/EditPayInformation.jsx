269efa9d17a83cbc20c1275757627684e455a637
REFH4CK <refh4ck.dev@gmail.com>
2025-07-24 19:34:37 -0400

    Edit pay information basic structure

---
+++ frontend/src/pages/admin/EditPayInformation.jsx
@@ -0,0 +1,13 @@
+import GoBack from "@/components/GoBack";
+
+export default function EditPayInformation() {
+  return (
+    <>
+      <section className="max-w-[768px] mx-auto flex flex-col items-center justify-center pt-4">
+        <div className="flex w-full max-w-[500px] px-2">
+          <GoBack></GoBack>
+        </div>
+      </section>
+    </>
+  );
+}
