df779847912dbba3c2536f4bceea297806657992
REFH4CK <refh4ck.dev@gmail.com>
2025-07-29 21:04:27 -0400

    modify toaster position

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,6 @@ import { Toaster } from "@pheralb/toast";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    <Toaster maxToasts={1}/>
+    <Toaster maxToasts={1} position="top-right"/>
   </StrictMode>
 );
