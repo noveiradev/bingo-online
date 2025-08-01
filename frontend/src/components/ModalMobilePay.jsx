144c68b727fac4b24b6366d10de3e5c2c366604f
REFH4CK <refh4ck.dev@gmail.com>
2025-08-01 12:23:44 -0400

    improve and complete payment modal

--- frontend/src/components/ModalMobilePay.jsx
+++ frontend/src/components/ModalMobilePay.jsx
@@ -1,7 +1,38 @@
 import BankCenter from "@/icons/BankCenter";
 import { Slide } from "react-awesome-reveal";
+import { payInformation } from "@/services/api/admin/payInformation";
+import { useState, useEffect } from "react";
+
+import { Tailspin } from "ldrs/react";
+import "ldrs/react/Tailspin.css";
 
 export default function ModalMobilePay({ modalFunction, payData }) {
+  const [activePayment, setActivePayment] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const paymentData = async () => {
+      try {
+        const response = await payInformation.activePayment();
+
+        if (response) {
+          setActivePayment(response.data);
+          setIsLoading(false);
+        } else {
+          console.error("No se encontraron datos de pago móvil activos");
+        }
+      } catch (error) {
+        console.error("Error al obtener los datos de pago:", error);
+      } finally {
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 1500);
+      }
+    };
+
+    paymentData();
+  }, []);
+
   return (
     <>
       <section className="bg-[#000]/70 absolute z-10 top-0 left-0 w-full h-dvh">
@@ -17,21 +48,46 @@ export default function ModalMobilePay({ modalFunction, payData }) {
                 Datos de pago móvil
               </h1>
               <div className="mx-auto pt-4">
-                <BankCenter w="90" h="90" color="#FAF2E795" />
+                <BankCenter w="90" h="90" color="#FFFFFF" />
               </div>
               <div className="w-full bg-white/8 h-[8rem] rounded-xl outline-simple-gold/50 outline-3 p-3">
-                <p className="text-white/75">
-                  <span className="text-light-gold/95 font-medium">Nombres:</span> {/*Database info*/}
-                </p>
-                <p className="text-white/75">
-                  <span className="text-light-gold/95 font-medium">Cédula:</span> {/*Database info*/}
-                </p>
-                <p className="text-white/75">
-                  <span className="text-light-gold/95 font-medium">Teléfono:</span> {/*Database info*/}
-                </p>
-                <p className="text-white/75">
-                  <span className="text-light-gold/95 font-medium">Banco:</span> {/*Database info*/}
-                </p>
+                {isLoading ? (
+                  <div className="flex justify-center items-center h-full">
+                    <Tailspin
+                      width={40}
+                      height={40}
+                      color="#DD8D1B"
+                      ariaLabel="Cargando..."
+                    />
+                  </div>
+                ) : (
+                  <>
+                    <p className="text-white/75">
+                      Nombres:{" "}
+                      <span className="text-gold/85 font-semibold font-inter">
+                        {activePayment?.name}
+                      </span>
+                    </p>
+                    <p className="text-white/75">
+                      Cédula:{" "}
+                      <span className="text-gold/85 font-semibold font-inter">
+                        {activePayment?.id_number}
+                      </span>
+                    </p>
+                    <p className="text-white/75">
+                      Teléfono:{" "}
+                      <span className="text-gold/85 font-semibold font-inter">
+                        {activePayment?.phone}
+                      </span>
+                    </p>
+                    <p className="text-white/75">
+                      Banco:{" "}
+                      <span className="text-gold/85 font-semibold font-inter">
+                        {activePayment?.bank}
+                      </span>
+                    </p>
+                  </>
+                )}
               </div>
             </div>
             <div className="px-4 flex flex-col">
