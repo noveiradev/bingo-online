dbccb9c9c9f663376138903ca551889d9da3ac2f
noveira <amylopez303@gmail.com>
2025-08-26 13:13:41 -0400

    fix: correct accentuation in the footer text

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="relative z-20 bottom-0 flex flex-col justify-center items-center w-full h-[5rem] bg-borgon text-center text-white/85 p-2">
       <p className="text-[0.85rem] stable:text-[0.95rem]">
-        Pagina desarrollada por <b>noveiradev</b> y <b>REFH4CK</b>
+        Página desarrollada por <b>noveiradev</b> y <b>REFH4CK</b>
       </p>
       <p className="text-[0.9rem]">Todos los derechos reservados © 2025</p>
       <CasinoLights
